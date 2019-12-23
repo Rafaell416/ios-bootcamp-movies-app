75a9101381fbcd1bb9235c3be0783411531516fe
Rafael Villarreal <rvillarreal416@gmail.com>
2019-12-23 11:43:47 -0500

    api key and base url added to the project

---
+++ movies-app/Utils/Config.swift
@@ -0,0 +1,14 @@
+//
+//  Config.swift
+//  movies-app
+//
+//  Created by Rafael Villarreal on 12/23/19.
+//  Copyright © 2019 Rafael Villarreal. All rights reserved.
+//
+
+import Foundation
+
+class Config {
+    static let baseURL: String = "https://api.themoviedb.org/3/movie"
+    static let apiKey: String = "ee91580503314066ff8ccb125431e5e4"
+}
